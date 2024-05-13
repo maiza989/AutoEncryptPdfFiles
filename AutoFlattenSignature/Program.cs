b7fb23f233f19694c3e0e2e949ebdfeefb16ac35
Maitham Alghamgham <maitham_gh98@hotmail.com>
2024-05-13 10:49:34 -0400

    Maitham | Restructured the file saving logic. - Added stronger password encryption. - Added comments to new code.

--- AutoFlattenSignature/Program.cs
+++ AutoFlattenSignature/Program.cs
@@ -7,6 +7,9 @@ class Program
         string inputFolderPath = @"ORIGINAL_FOLDER_YOU_WANT_TO_PROCESS";                                                                                                // Path of original input PDFs folder
         string outputFolderPath = @"OUTPUT_FOLDER_OF_PROCESSED_FILES";                                                                                                  // Destination path for output of processed PDFs files
         string errorFolderPath = @"ERROR_FOLDER";                                                                                                                       // Destination path for error files if any encounter errors. 
+        string ownerPassword = "a3JBt93q^X3Z&S8*f#Q&HZUeEFYGA!";                                                                                                        // Encryption password. *** choose your own ***
+        string openPassword = "72PDB&fi8QunYt9svHuRjXqZYqJv^#";                                                                                                         // *** if you want open selected view to open the document with already know password.
+                                                                                                                                                                        // Replace "String.Empty" with openPassword Variable. %Line36% ***
 
         if (!Directory.Exists(inputFolderPath))                                                                                                                         // Check if the input and output folders exist
         {
@@ -30,16 +33,16 @@ class Program
 
                 if (pdf != null)                                                                                                                                        // Check if the document is successfully loaded
                 {
-                    pdf.Security.Encrypt(string.Empty,"1",PdfPermissionsFlags.FillFields,PdfEncryptionKeySize.Key128Bit);                                               // Encrypt the folder so its not editable. "Open and Permission password cannot be the same"
-                    string outputFile = Path.Combine(outputFolderPath, Path.GetFileName(pdfFile));                                                                      // Save the modified PDF to the output folder
-                    pdf.SaveToFile(outputFile);
-                    Console.WriteLine($"\tEncrypting file completed for: {Path.GetFileName(pdfFile)}");
+                    pdf.Security.Encrypt(string.Empty, ownerPassword, PdfPermissionsFlags.None,PdfEncryptionKeySize.Key128Bit);                                         // Encrypt the folder so its not editable. "Open and Permission password cannot be the same"
                 }// end of if-statement
                 else
                 {
                     Console.WriteLine($"Error processing file {Path.GetFileName(pdfFile)}: PDF document or its form is null.");
                     File.Move(inputFolderPath, errorFolderPath);                                                                                                        // Moving error file.                                                                                         
                 }// end of else-statement
+                    string outputFile = Path.Combine(outputFolderPath, Path.GetFileName(pdfFile));                                                                      // Save the modified PDF to the output folder
+                    pdf.SaveToFile(outputFile);
+                    Console.WriteLine($"\tEncrypting file completed for: {Path.GetFileName(pdfFile)}");
             }// end of try-catch
             catch (Exception ex)
             {
